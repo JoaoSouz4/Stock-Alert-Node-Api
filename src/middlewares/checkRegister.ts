8224f44f54e37965b6eeb31677504c3309580b2b
João Vitor Souza <jvcsouza12@gmail.com>
2023-09-12 14:43:03 -0300

    .

---
+++ src/middlewares/checkRegister.ts
@@ -0,0 +1,22 @@
+import {NextFunction, Request, Response} from 'express';
+import Product from '../models/ProductModel';
+
+export async function checkRegister(req: Request, res: Response, next: NextFunction){
+    const {name, registedBy, status, categorie} = req.body;
+    const hasRegister = await namehasRegisted(name, categorie);
+    if(hasRegister){
+        return res.status(400).json({
+            isSucess: false,
+            requestMessage: 'Já existe um produto registrado nesta categoria'
+        })
+    }
+
+    next();
+}
+
+async function namehasRegisted(name: string, categorie: string){
+    
+    const hasRegister = await Product.findOne({name: name, categorie: categorie});
+    console.log(hasRegister)
+    return hasRegister? true : false;
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import ProductController from "./controllers/productController";
-
+import { checkRegister } from "./middlewares/checkRegister";
 const routes = Router();
 
 
-routes.post('/post/registerproduct', ProductController.Register);
+routes.post('/post/registerproduct', checkRegister, ProductController.Register);
 
 routes.get('/get/getallproducts', ProductController.getAllProducts)
 routes.get('/get/getonelist/:namelist', ProductController.getOneList)
